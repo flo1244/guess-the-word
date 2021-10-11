67786dd789b18a861aff38735b66ebc5bc374c26
flo_h <flo.1244@gmail.com>
2021-10-10 20:43:59 -0400

    Phase 1

---
+++ js/script.js
@@ -0,0 +1,35 @@
+//Javascript Document
+
+const guessedLetters = document.querySelector(".guessed-letters"); //ul guessed letters
+const guessButton = document.querySelector(".guess"); //guess button
+const letterInput = document.querySelector(".letter"); //input letter box
+const wordProgress = document.querySelector(".word-in-progress"); //p where the word in progress will appear
+const remainingGuess = document.querySelector(".remaining"); //p remaining guess will appear
+const numGuesses = document.querySelector("span"); //span in remaining guess shows number 
+const message = document.querySelector(".message"); //display message when letter is guessed
+const playAgain = document.querySelector(".play-again"); //Play again button
+
+const word = "magnolia"; //test purposes
+
+//Adds placeholder for each letter func.
+const placeholder = function (word) {
+	const letterHolder = []; //empty array which will be filled with same number of circles as letters once it's looped.
+	for(const letter of word){
+		console.log(letter);
+		letterHolder.push = ("●");
+	}
+	wordProgress.innerText = letterHolder.join("");
+};
+
+placeholder(word);
+
+//Captures letter input when clicked.
+guessButton.addEventListener("click", function (e){
+	e.preventDefault();
+	const captureLetter = letterInput.value;
+	console.log(captureLetter);
+	letterInput.value = "";
+});
+
+
+
